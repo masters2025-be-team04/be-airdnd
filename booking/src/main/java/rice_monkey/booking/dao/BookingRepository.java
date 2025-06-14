c02d7883ed0c7a77a2db487d7111ba3ff095e76c
HeeSung Choi <gladhee@icloud.com>
2025-06-14 23:19:08 +0900

    refactor(BookingRepository): 사용하지 않는 메소드 삭제

--- booking/src/main/java/rice_monkey/booking/dao/BookingRepository.java
+++ booking/src/main/java/rice_monkey/booking/dao/BookingRepository.java
@@ -2,8 +2,6 @@ package rice_monkey.booking.dao;
 
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
-
 @Repository
 public interface BookingRepository extends JpaBookingRepository {
 
--- booking/src/main/java/rice_monkey/booking/dao/JpaBookingRepository.java
+++ booking/src/main/java/rice_monkey/booking/dao/JpaBookingRepository.java
@@ -3,8 +3,5 @@ package rice_monkey.booking.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rice_monkey.booking.domain.Booking;
 
-import java.util.Optional;
-
 public interface JpaBookingRepository extends JpaRepository<Booking, Long> {
-    Optional<Booking> findByIdAndGuestId(Long id, Long guestId);
 }
