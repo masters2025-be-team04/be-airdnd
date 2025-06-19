4b5cce62df979121639c17e6a90c472bd66cdfa6
HeeSung Choi <gladhee@icloud.com>
2025-06-20 00:57:35 +0900

    refactor(BookingEvent): 이벤트 관련 로직 제거

--- booking/src/main/java/rice_monkey/booking/service/BookingService.java
+++ booking/src/main/java/rice_monkey/booking/service/BookingService.java
@@ -1,11 +1,8 @@
 package rice_monkey.booking.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import rice_monkey.booking.common.constants.BookingConstant;
 import rice_monkey.booking.dao.BookingRepository;
 import rice_monkey.booking.domain.Booking;
 import rice_monkey.booking.domain.BookingState;
@@ -15,7 +12,6 @@ import rice_monkey.booking.dto.response.BookingResponseDto;
 import rice_monkey.booking.exception.business.Authorization.UnauthorizedBookingAccessException;
 import rice_monkey.booking.exception.business.booking.AlreadyBookedException;
 import rice_monkey.booking.exception.business.booking.BookingNotFoundException;
-import rice_monkey.booking.exception.infra.JsonSerializationException;
 import rice_monkey.booking.feign.listing.ListingClient;
 import rice_monkey.booking.feign.listing.dto.ListingDto;
 
@@ -24,9 +20,7 @@ import rice_monkey.booking.feign.listing.dto.ListingDto;
 public class BookingService {
 
     private final BookingRepository bookingRepository;
-    private final BookingEventRepository bookingEventRepository;
     private final ListingClient listingClient;
-    private final RedisLockService redisLockService;
 
     @Transactional
     public BookingReserveResponseDto reserve(BookingReserveRequestDto dto, Long guestId) {
@@ -48,6 +42,7 @@ public class BookingService {
         if (booking.getGuestId() != userId) {
             throw new UnauthorizedBookingAccessException(id, userId);
         }
+
         return BookingResponseDto.from(booking);
     }
 
@@ -60,11 +55,7 @@ public class BookingService {
             throw new UnauthorizedBookingAccessException(id, userId);
         }
 
-        booking.setStatus(BookingState.CANCELED);
-
-        BookingEvent bookingEvent = safeCreateBookingEvent(booking, BookingConstant.BOOKING_CANCELED);
-        bookingEventRepository.save(bookingEvent);
-
+        booking.updateState(BookingState.CANCELED);
         bookingRepository.delete(booking);
     }
 
@@ -73,18 +64,8 @@ public class BookingService {
         Booking booking = bookingRepository.findById(id)
                 .orElseThrow(() -> new BookingNotFoundException(id));
 
-        booking.setStatus(BookingState.CONFIRMED);
-        BookingEvent bookingEvent = safeCreateBookingEvent(booking, BookingConstant.BOOKING_CONFIRMED);
-
-        bookingEventRepository.save(bookingEvent);
-    }
-
-    private BookingEvent safeCreateBookingEvent(Booking booking, String eventType) {
-        try {
-            return BookingEvent.of(eventType, booking);
-        } catch (JsonProcessingException e) {
-            throw new JsonSerializationException("Error serializing booking event data");
-        }
+        booking.updateState(BookingState.CONFIRMED);
+        bookingRepository.save(booking);
     }
 
 }
