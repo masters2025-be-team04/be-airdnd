7ce760667d7501e42ba0f755df0c07bf997ba210
HeeSung Choi <gladhee@icloud.com>
2025-06-20 00:59:14 +0900

    feat(RedissonConfig): Redisson 비밀번호 정책 변경 및 버전 업데이트

--- booking/src/main/java/rice_monkey/booking/common/config/RedissonConfig.java
+++ booking/src/main/java/rice_monkey/booking/common/config/RedissonConfig.java
@@ -4,27 +4,35 @@ import lombok.RequiredArgsConstructor;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedissonConfig {
 
+    private final RedisProperties redisProperties;
 
-    @Bean(destroyMethod = "shutdown")
-    public RedissonClient redisson(@Value("${spring.redis.host}") String host,
-                                   @Value("${spring.redis.port}") int port) {
-        Config config = new Config();
-        config.setThreads(4).setNettyThreads(4);
-
-        config.useSingleServer()
-                .setAddress("redis://" + host + ":" + port)
+    @Bean(name = "redisson", destroyMethod = "shutdown")
+    @Primary
+    public RedissonClient redisson() {
+        Config cfg = new Config();
+        var single = cfg
+                .useSingleServer()
+                .setAddress("redis://" +
+                        redisProperties.getHost() + ":" +
+                        redisProperties.getPort())
                 .setConnectionMinimumIdleSize(8)
                 .setTimeout(3000);
 
-        return Redisson.create(config);
+        // 비밀번호가 설정되어 있을 때만 AUTH
+        String pw = redisProperties.getPassword();
+        if (pw != null && !pw.isBlank()) {
+            single.setPassword(pw);
+        }
+        return Redisson.create(cfg);
     }
 
 }
