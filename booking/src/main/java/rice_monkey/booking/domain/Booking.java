10a26e95e47ec0c8565bb2147a782697a0dd1816
HeeSung Choi <gladhee@icloud.com>
2025-06-19 20:09:22 +0900

    refactor(Booking): @Column 어노테이션 추가해서 ddl 추가

--- booking/src/main/java/rice_monkey/booking/domain/Booking.java
+++ booking/src/main/java/rice_monkey/booking/domain/Booking.java
@@ -18,16 +18,34 @@ public class Booking extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "listing_id", nullable = false)
     private Long listingId;
+
+    @Column(name = "guest_id", nullable = false)
     private Long guestId;
+
+    @Column(name = "checkin_at", nullable = false, columnDefinition = "DATE")
     private LocalDate checkinAt;
+
+    @Column(name = "checkout_at", nullable = false, columnDefinition = "DATE")
     private LocalDate checkoutAt;
+
+    @Column(name = "nights", nullable = false)
     private Integer nights;
+
+    @Column(name = "guest_count", nullable = false)
     private Integer guestCount;
+
+    @Column(name = "payment_amount", nullable = false)
     private Integer paymentAmount;
+
+    @Column(name = "listing_title_snapshot", nullable = false)
     private String listingTitleSnapshot;
+
     @Builder.Default
     @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
     private BookingState status = BookingState.REQUESTED;
 
 }
