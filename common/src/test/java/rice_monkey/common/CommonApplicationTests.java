df475ba2686c73e0015eca7e7655a61d5b4202fe
HeeSung Choi <gladhee@icloud.com>
2025-06-21 21:00:13 +0900

    init(common): 공통로직 모아놓는 common 모듈 추가

---
+++ common/src/test/java/rice_monkey/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package rice_monkey.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
